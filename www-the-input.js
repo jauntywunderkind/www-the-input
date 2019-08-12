c1af3ee3071900dab4e351b4f21e3bb3e830b64e
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-08-12 14:02:13 -0400

    Initial sketch out of capabilities

---
+++ www-the-input.js
@@ -0,0 +1,18 @@
+<html>
+<head>
+</head>
+<body>
+<theinput dest="/post">
+
+<div id="canvas" name="canvas"></div>
+<input type=text id="text" name="text"></input>
+<button id="sound" name="sound">sound</button>
+<button id="photo" name="photo">photo</button>
+<button id="video" name="video">video</button>
+<input type=text id="unicode" name="unicode"></input>
+
+<div id="previews"></div>
+
+</theinput>
+</body>
+</html>
